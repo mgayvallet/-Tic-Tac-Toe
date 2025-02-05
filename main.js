818ae9efae3fa0d3506a2c65b7267f4186476459
qwyx <mc010116@gmail.com>
2025-02-05 11:59:31 +0100

    feat(app): finished home

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
     const retour = document.querySelector('.returnDiv');
-    const cpuPlay = document.querySelector('.CPU');
-    if (cpuPlay){
-        cpuPlay.addEventListener('click', () => {
-            window.location.href = 'games.html';
-        });
-    }
+    const mainGame = document.querySelector('#mainGame');
+    const overlayST = document.querySelector('.overlay-start-game');
+    const overlayGame = document.querySelector('.overlayGame');
+    const cpu = document.querySelector('.CPU');
+    const boxes = document.querySelectorAll('.box');
+    const turnDisplay = document.querySelector('.pTurn');
+    const divX = document.querySelector('.divX');
+    const divCercle = document.querySelector('.divCercle');
+    const imgDivX = document.querySelector('.imgDivX');
+    const imgDivCercle = document.querySelector('.imgDivCercle');
+    const goFirst = document.querySelector('.goFirst');
 
-    if (retour) {
-        retour.addEventListener('click', () => {
-            window.location.href = 'index.html';
-        });
-    }
+    divX.addEventListener('click', () => {
+        divX.style.backgroundColor = '#A8BFC9';
+        divCercle.style.backgroundColor = 'transparent';
+        imgDivX.src = "assets/Combined Shape Copy 2 black.svg";
+        imgDivCercle.src = "assets/Oval Copy white.svg";
+        goFirst.textContent = 'X'
+    });
+
+    divCercle.addEventListener('click', () => {
+        divCercle.style.backgroundColor = '#A8BFC9';
+        divX.style.backgroundColor = 'transparent';
+        imgDivX.src = "assets/Combined Shape Copy 2.svg";
+        imgDivCercle.src = "assets/Oval Copy.svg";
+        goFirst.textContent = 'O'
+    });
+
+    cpu.addEventListener('click', () => {
+        overlayST.style.display = 'none';
+        overlayGame.style.display = 'block';
+    });
+
 });
